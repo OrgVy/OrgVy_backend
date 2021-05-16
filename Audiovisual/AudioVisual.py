c4aef08b6f5ab578e0a3a32123e116132e14ab3e
oalvarezr <oalvarezr@unal.edu.co>
2021-05-15 20:27:42 -0500

    add connection and logger

--- Audiovisual/AudioVisual.py
+++ Audiovisual/AudioVisual.py
@@ -3,6 +3,11 @@ from flask import Blueprint
 AudioVisual = Blueprint('AudioVisual', __name__, url_prefix="/audio-visual")
 
 
+@AudioVisual.route("/")
+def main():
+    return "main"
+
+
 @AudioVisual.route("/hello")
 def accountList():
     return "hello audiovisual"
