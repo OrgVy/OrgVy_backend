cce1d8f4db3af772160f7bbc33adf98894da24a4
oalvarezr <oalvarezr@unal.edu.co>
2021-05-17 23:56:54 -0500

    implemented post and put for Category and Type

--- Audiovisual/Category.py
+++ Audiovisual/Category.py
@@ -4,7 +4,8 @@ from . import Category
 import Database.connectionDB as connectionDB
 
 
-@Category.route("", methods=['GET'])
+# a for PUT method use format Old=value1&New=value2
+@Category.route("", methods=['GET', 'PUT'])
 def get_categories():
     if request.method == 'GET':
         try:
@@ -20,5 +21,38 @@ def get_categories():
             return {"categories": aux}, 200
         except:
             return 500
+    elif request.method == 'PUT':
+        try:
+            old = request.args['Old']
+            new = request.args['New']
+
+            sql = "UPDATE `OrgVy`.`category` SET `name`='" + new + "' WHERE `name`='" + old + "';"
+            connection = connectionDB.open_connection()
+            connection_cursor = connection.cursor()
+            connection_cursor.execute(sql)
+            connection.commit()
+            connection_cursor.close()
+            connection.close()
+
+            return "", 200
+        except:
+            return 500
+    else:
+        return "405 Method Not Allowed", 405
+
+
+@Category.route("/<category>", methods=['POST'])
+def post_category(category):
+    if request.method == 'POST':
+        try:
+            connection = connectionDB.open_connection()
+            connection_cursor = connection.cursor()
+            connection_cursor.execute("INSERT INTO `OrgVy`.`category` (`name`) VALUES ('" + category + "');")
+            connection.commit()
+            connection_cursor.close()
+            connection.close()
+            return "", 200
+        except:
+            return "", 400
     else:
         return "405 Method Not Allowed", 405
--- Audiovisual/Type.py
+++ Audiovisual/Type.py
@@ -3,7 +3,8 @@ from flask import request
 import Database.connectionDB as connectionDB
 
 
-@Type.route("", methods=['GET'])
+# a for PUT method use format Old=value1&New=value2
+@Type.route("", methods=['GET', 'PUT'])
 def get_types():
     if request.method == 'GET':
         try:
@@ -19,5 +20,38 @@ def get_types():
             return {"types": aux}, 200
         except:
             return 500
+    elif request.method == 'PUT':
+        try:
+            old = request.args['Old']
+            new = request.args['New']
+
+            sql = "UPDATE `OrgVy`.`type` SET `name`='" + new + "' WHERE `name`='" + old + "';"
+            connection = connectionDB.open_connection()
+            connection_cursor = connection.cursor()
+            connection_cursor.execute(sql)
+            connection.commit()
+            connection_cursor.close()
+            connection.close()
+
+            return "", 200
+        except:
+            return 500
+    else:
+        return "405 Method Not Allowed", 405
+
+
+@Type.route("/<type>", methods=['POST'])
+def post_category(type):
+    if request.method == 'POST':
+        try:
+            connection = connectionDB.open_connection()
+            connection_cursor = connection.cursor()
+            connection_cursor.execute("INSERT INTO `OrgVy`.`type` (`name`) VALUES ('" + type + "');")
+            connection.commit()
+            connection_cursor.close()
+            connection.close()
+            return "", 200
+        except:
+            return "", 400
     else:
         return "405 Method Not Allowed", 405
